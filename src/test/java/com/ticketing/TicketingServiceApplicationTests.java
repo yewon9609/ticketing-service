fa82ba2d86ccf281a2c50bcd342152c0c1ecb659
yewon.seo <dodnjs1241@naver.com>
2023-06-12 23:26:07 +0900

    환경설정

---
+++ src/test/java/com/ticketing/TicketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
