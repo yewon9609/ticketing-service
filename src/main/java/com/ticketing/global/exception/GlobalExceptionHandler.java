10b4f62679e2877a91a8773f82216922df0a5036
yewon.seo <dodnjs1241@naver.com>
2023-06-16 15:10:06 +0900

    feat: exceptionHandler추가

--- src/main/java/com/ticketing/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ticketing/global/exception/GlobalExceptionHandler.java
@@ -1,29 +1,68 @@
 package com.ticketing.global.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
   private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+  @ExceptionHandler(BusinessException.class)
+  public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+    log.info("BusinessException: ", e);
+    ErrorCode errorCode = e.getErrorCode();
+    return ResponseEntity.status(errorCode.getCode())
+        .body(new ErrorResponse(errorCode.getMessage()));
+  }
 
   @ExceptionHandler(IllegalArgumentException.class)
   public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
     log.info("IllegalArgumentException: ", e);
-    String message = e.getMessage();
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(message));
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(new ErrorResponse(e.getMessage()));
+  }
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException e) {
+    log.info("EntityNotFoundException: ", e);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(new ErrorResponse(e.getMessage()));
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<ErrorResponse> handleIllegalArgumentException(MethodArgumentNotValidException e) {
+  public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException e) {
     log.info("MethodArgumentNotValidException: ", e);
-    String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(message));
+    StringBuilder builder = new StringBuilder();
+
+    for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+      builder.append(fieldError.getField());
+      builder.append("(은)는 ");
+      builder.append(fieldError.getDefaultMessage());
+    }
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(new ErrorResponse(builder.toString()));
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+    log.error("RuntimeException : ", e);
+    return ResponseEntity.internalServerError().build();
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleException(Exception e) {
+    log.error("Exception : ", e);
+    return ResponseEntity.internalServerError().build();
   }
 
 }
