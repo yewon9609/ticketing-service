dd400b47d8753e7b938f54ba868f42d81755a3c6
yewon.seo <dodnjs1241@naver.com>
2023-06-27 21:25:31 +0900

    feat: 직렬화 이슈 해결

--- src/main/java/com/ticketing/domain/performance/entity/Schedule.java
+++ src/main/java/com/ticketing/domain/performance/entity/Schedule.java
@@ -1,5 +1,11 @@
 package com.ticketing.domain.performance.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.time.LocalDate;
@@ -11,9 +17,14 @@ public class Schedule {
 
   private static final String CANNOT_BEFORE_NOW = "공연 시작 날짜는 오늘날짜보다 이전일 수 없습니다";
 
+
+  @JsonSerialize(using = LocalDateSerializer.class)
+  @JsonDeserialize(using = LocalDateDeserializer.class)
   @Column(nullable = false)
   private LocalDate startDate;
 
+  @JsonSerialize(using = LocalTimeSerializer.class)
+  @JsonDeserialize(using = LocalTimeDeserializer.class)
   @Column(nullable = false)
   private LocalTime startTime;
 
