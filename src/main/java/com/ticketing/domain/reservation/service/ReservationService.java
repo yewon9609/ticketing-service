d59016f5be4d190410fd52f2da3fb552a973c093
yewon.seo <dodnjs1241@naver.com>
2023-06-27 21:23:32 +0900

    refactor: performance serivce에 책임 분리

--- src/main/java/com/ticketing/domain/reservation/service/ReservationService.java
+++ src/main/java/com/ticketing/domain/reservation/service/ReservationService.java
@@ -10,9 +10,8 @@ import com.ticketing.domain.reservation.dto.response.ReservationCreateRes;
 import com.ticketing.domain.reservation.dto.response.ReservationDetailRes;
 import com.ticketing.domain.reservation.dto.response.ReservationSimpleRes;
 import com.ticketing.domain.reservation.entity.Reservation;
-import com.ticketing.domain.reservation.exception.NotFoundReservationException;
+import com.ticketing.domain.reservation.exception.ReservationNotFoundException;
 import com.ticketing.domain.reservation.repository.ReservationRepository;
-import com.ticketing.global.exception.ErrorCode;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
@@ -35,11 +34,14 @@ public class ReservationService {
 
   @DistributedLock(key = "#createReq.performanceId")
   public ReservationCreateRes create(ReservationCreateReq createReq, Long customerId) {
-    Customer customer = customerService.getBy(customerId);
-    Performance performance = checkAvailabilityOfReservations(createReq, customer);
+    Customer customer = customerService.getById(customerId);
 
-    performance.getSeat()
-        .subtractBy(createReq.ticketCount());
+    Performance performance = performanceService.subtractSeats(
+        createReq.performanceId(),
+        customer.getAge(),
+        createReq.ticketCount()
+    );
+
     Reservation reservation = reservationRepository.save(
         createReq.toEntity(performance, customer)
     );
@@ -47,17 +49,9 @@ public class ReservationService {
     return ReservationCreateRes.from(reservation);
   }
 
-  private Performance checkAvailabilityOfReservations(ReservationCreateReq createReq,
-      Customer customer) {
-    Performance performance = performanceService.get(createReq.performanceId());
-    performance.checkPossibleViewingAge(customer.getMemberInfo().getAge());
-
-    return performance;
-  }
-
   public ReservationDetailRes getDetails(Long id, Long customerId) {
     return reservationRepository.getDetail(id, customerId)
-        .orElseThrow(() -> new NotFoundReservationException(ErrorCode.NOT_FOUND_RESERVATION));
+        .orElseThrow(ReservationNotFoundException::new);
   }
 
   public Slice<ReservationSimpleRes> getList(Long customerId, Pageable pageable) {
